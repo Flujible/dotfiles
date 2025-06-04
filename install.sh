0a01429c4ef8ad51a4d09497d0b8e3e7604d6474
George <georgepalmerbryant@gmail.com>
2025-06-04 16:00:01 +0100

    refactor: add taskwarrior to install file

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -s ~/.dotfiles/gitconfig/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.zprofile ~/.zprofile
+ln -s ./.dotfiles/taskwarrior/.taskrc ~/.taskrc 
