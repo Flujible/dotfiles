951747ac5009a169f14f53749083880dcb73d510
George Palmer-Bryant <georgepalmerbryant@gmail.com>
2026-04-20 16:39:37 +0100

    refactor: add logs for symlink creation

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+echo "Setting up symlinks..."
 ln -sf ~/.dotfiles/gitconfig/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/.dotfiles/taskwarrior/.taskrc ~/.taskrc
+echo "Symlinks created."
 
 source ~/.dotfiles/scripts/build_helpers.sh
 
